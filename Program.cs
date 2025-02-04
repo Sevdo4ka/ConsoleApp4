e4fdb7b230066e5997db0853f01974abc2d84ef5
Sevdo4ka <castilleal_fred@inbox.ru>
2025-02-04 11:24:20 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,56 +10,75 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("a)");
-            for (int i = 0; i < 4; i++) 
-            {
-                for (int j = 1; j <= 10; j++) 
-                {
-                    Console.Write(j + " ");
-                }
-                Console.WriteLine();
-            }
+            //Task 1
+            //Console.WriteLine("a)");
+            //for (int i = 0; i < 4; i++) 
+            //{
+            //    for (int j = 1; j <= 10; j++) 
+            //    {
+            //        Console.Write(j + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("b)");
+            //int num = 41; 
+            //for (int i = 0; i < 4; i++) 
+            //{
+            //    for (int j = 0; j < 10; j++) 
+            //    {
+            //        Console.Write(num + " ");
+            //        num++; 
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("в)");
+            //for (int i = 1; i <= 6; i++) 
+            //{
+            //    for (int j = 0; j < i; j++) 
+            //    {
+            //        Console.Write("5 ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("г)");
+            //for (int i = 6; i > 0; i--) 
+            //{
+            //    for (int j = 0; j < i; j++) 
+            //    {
+            //        Console.Write("1 ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.ReadKey();
 
-            Console.WriteLine(); 
+            //Task 2
+            //Console.Write("#######\f#######\f#######\f#######");
+            //Console.ReadKey();
 
-            
-            Console.WriteLine("b)");
-            int num = 41; 
-            for (int i = 0; i < 4; i++) 
-            {
-                for (int j = 0; j < 10; j++) 
-                {
-                    Console.Write(num + " ");
-                    num++; 
-                }
-                Console.WriteLine();
-            }
+            //Task 3
+            //Console.Write("################\n");
+            //Console.Write("#              #\n");
+            //Console.Write("#              #\n");
+            //Console.Write("#              #\n");
+            //Console.Write("#              #\n");
+            //Console.Write("#              #\n");
+            //Console.Write("#              #\n");
+            //Console.Write("#              #\n");
+            //Console.Write("#              #\n");
+            //Console.Write("#              #\n");
+            //Console.Write("#              #\n");
+            //Console.Write("#              #\n");
+            //Console.Write("#              #\n");
+            //Console.Write("#              #\n");
+            //Console.Write("################\n");
+            //Console.ReadKey();
 
-            Console.WriteLine();
-
-            
-            Console.WriteLine("в)");
-            for (int i = 1; i <= 6; i++) 
-            {
-                for (int j = 0; j < i; j++) 
-                {
-                    Console.Write("5 ");
-                }
-                Console.WriteLine();
-            }
-
-            Console.WriteLine();
-
-            
-            Console.WriteLine("г)");
-            for (int i = 6; i > 0; i--) 
-            {
-                for (int j = 0; j < i; j++) 
-                {
-                    Console.Write("1 ");
-                }
-                Console.WriteLine();
-            }
+            //Task 4
+            Console.Write("#\f #\f  #\f   #\f    #\f     #\f      #\f       #");
+            Console.ReadKey();
         }
     }
 }
